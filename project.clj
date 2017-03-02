7dd8e3cc20bb8e14d35f7e2c9a99e735d33721bf
shavit <shavit@users.noreply.github.com>
2017-03-01 21:57:01 -0500

    🛫

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   ; Set the entry point
-  :main clojure-lisp.core/start)
+  :main clojure-lisp.core)
--- src/clojure_lisp/core.clj
+++ src/clojure_lisp/core.clj
@@ -1,7 +1,11 @@
 (ns clojure-lisp.core)
 
-(defn start
-  "I don't do a whole lot."
-  []
-  (println "Hello, World!")
+(defn start-app
+  [args]
+  (println "Starting app"))
+
+(defn -main
+  "Starting the app with argumens"
+  [& args]
+  (start-app args)
   )
